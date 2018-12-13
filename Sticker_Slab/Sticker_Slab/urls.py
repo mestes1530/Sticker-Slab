24fc801a69dd3fe0faff6179f35d15475ea0bfb8
Mitchell <msestes1530@gmail.com>
2018-12-13 20:23:19 +0000

    adding server changes

--- Sticker_Slab/Sticker_Slab/urls.py
+++ Sticker_Slab/Sticker_Slab/urls.py
@@ -7,4 +7,4 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('slab.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
